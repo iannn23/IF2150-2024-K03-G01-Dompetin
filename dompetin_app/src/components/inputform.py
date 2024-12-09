5c8dbee02e6140a6cd117268389f590bc257029e
jonathankenan <168574976+jonathankenan@users.noreply.github.com>
2024-12-09 16:50:50 +0700

    feat : menyelesaikan UI inputform

--- dompetin_app/src/components/inputform.py
+++ dompetin_app/src/components/inputform.py
@@ -5,9 +5,9 @@ def InputFormView(page: ft.Page):
     
     def button_clicked(e):
         selected_amenity_value = selected_amenity.current.label.value if selected_amenity.current else "None"
-        t.value = f"Textboxes values are: Selected Date: {selected_date.current.value}, Judul : {tb1.value}, Jumlah nominal : {tb2.value}, Catatan tambahan : {tb3.value}, Selected Amenity : {selected_amenity_value}."
+        selected_type_value = selected_type.current.label.value if selected_type.current else "None"
+        t.value = f"Textboxes values are: Tipe : {selected_type_value}, Date: {selected_date.current.value}, Judul : {tb1.value}, Jumlah nominal : {tb2.value}, Catatan tambahan : {tb3.value}, Selected Amenity : {selected_amenity_value}."
         page.update()   
-
     selected_date = ft.Ref[ft.Text]()
     def handle_change(e):
         selected_date.current.value = e.control.value.strftime('%Y-%m-%d')
@@ -39,8 +39,29 @@ def InputFormView(page: ft.Page):
     titleCatatan = ft.Row([ft.Icon(ft.Icons.HOTEL_CLASS), ft.Text("Catatan")])
     tb3 = ft.TextField(label="Catatan", read_only=False, hint_text="Masukan catatan tambahan untuk transaksi")
 
-    tb5 = ft.TextField(label="With an icon", icon=ft.Icons.EMOJI_EMOTIONS)
-    
+    # tb5 = ft.TextField(label="With an icon", icon=ft.Icons.EMOJI_EMOTIONS)
+    selected_type = ft.Ref[ft.Chip]()
+    def type_selected(e):
+        for chip in type_chips:
+            chip.selected = False
+        e.control.selected = True
+        selected_type.current = e.control
+        page.update()
+
+    titleTipe = ft.Row([ft.Icon(ft.icons.HOTEL_CLASS), ft.Text("Tipe")])
+    types = ["Pengeluaran", "Penghasilan"]
+    type_chips = []
+
+    for type in types:
+        chip = ft.Chip(
+            label=ft.Text(type),
+            bgcolor=ft.colors.BLUE_200,
+            disabled_color=ft.colors.BLUE_100,
+            autofocus=True,
+            on_select=type_selected,
+        )
+        type_chips.append(chip)
+        
     selected_amenity = ft.Ref[ft.Chip]()
     def amenity_selected(e):
         for chip in amenity_chips:
@@ -49,7 +70,7 @@ def InputFormView(page: ft.Page):
         selected_amenity.current = e.control
         page.update()
 
-    title = ft.Row([ft.Icon(ft.icons.HOTEL_CLASS), ft.Text("Kategori")])
+    titleKategori = ft.Row([ft.Icon(ft.icons.HOTEL_CLASS), ft.Text("Kategori")])
     amenities = ["Belanja", "Makan", "Hiburan", "Transportasi", "Tagihan", "Penghasilan"]
     amenity_chips = []
 
@@ -69,13 +90,15 @@ def InputFormView(page: ft.Page):
         content=ft.ListView( 
             controls=[
                 ft.Text("Input Transaksi", size=30),
+                titleTipe,
+                ft.Row(type_chips),
                 titleDate,
                 ft.Row([date_picker_button, selected_date_display], alignment=ft.MainAxisAlignment.START),
                 titleJudul,
                 tb1,
                 titleJumlah,
                 tb2,
-                title,
+                titleKategori,
                 ft.Row(amenity_chips),
                 titleCatatan,
                 tb3,
